a2667898635c44a4ebdbe4ee5bb33498348c88b5
Bruno L. Carli <brunolcarli@gmail.com>
2021-02-19 15:02:35 -0300

    ADD docstrings na sala

--- core/room.py
+++ core/room.py
@@ -2,6 +2,11 @@ from random import randint
 
 
 class Room3D:
+    """
+    Uma sala é representada por um espaço cartesiano XY.
+    Uma sala deverá ter um nome de referência.
+    Por padrão as salas tem dimensão 5x5 podendo ser alterado
+    """
     def __init__(self, name, max_size_x=5, max_size_y=5):
         self.name = name
         self.max_size_x = max_size_x
@@ -9,7 +14,13 @@ class Room3D:
         self._map = self.build()
 
     def build(self):
+        """
+        Inicializa a sala com 0s e 1s.
+        """
         return [[randint(0, 1) for _ in range(self.max_size_x)] for _ in range(self.max_size_y)]
+
+    def __str__(self):
+        return f'Room {self.name}'
 
     def __getitem__(self, index):
         return self._map.__getitem__(index)
@@ -27,9 +38,9 @@ class Room3D:
         return self._map.__getitem__(index)
 
     def show(self):
-        print('[')
+        print('\t      N\n')
         for row in self._map:
             line = ' '.join(str(i) for i in row)
             print(f'\t[ {line} ]')
-        print(']')
+        print('\n\t      S')
 
